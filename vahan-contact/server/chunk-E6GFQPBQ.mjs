c0ebd4c22fb25b92de2765f0b93c3623fb72b59e
aniket gurav <aniketgurav2442@gmail.com>
2026-07-12 02:28:26 +0530

    frontend-changes

---
+++ vahan-contact/server/chunk-E6GFQPBQ.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+var o={production:!0,apiUrl:window.location.origin};export{o as a};
--- vahan-contact/server/main.server.mjs
+++ vahan-contact/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-CCK63KZG.mjs";import"./chunk-UEXMBV7T.mjs";import"./chunk-OWPOPSVK.mjs";import"./chunk-AYZKRYTH.mjs";import"./chunk-YKELGJXD.mjs";import"./chunk-HJHU3BOS.mjs";import"./chunk-SQBKGHJR.mjs";import"./chunk-FME56UVT.mjs";export{a as default};
+import{a}from"./chunk-OCQJXMIG.mjs";import"./chunk-UEXMBV7T.mjs";import"./chunk-5WMDB5Z3.mjs";import"./chunk-AYZKRYTH.mjs";import"./chunk-YKELGJXD.mjs";import"./chunk-7VE3RNII.mjs";import"./chunk-E6GFQPBQ.mjs";import"./chunk-SQBKGHJR.mjs";import"./chunk-FME56UVT.mjs";export{a as default};
